ff873446777c4d67b403e72266262b3e8ac50052
Lilypads <lilypads.lily@gmail.com>
2022-02-24 00:12:25 +0000

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -1,3 +1,7 @@
 #define BOOST_TEST_MODULE testModuleName
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
+
+BOOST_WARN(1==1); //warn and continue
+BOOST_CHECK(1>0); //fail but continue
+BOOST_REQUIRE(1!=2); //fail and terminate
