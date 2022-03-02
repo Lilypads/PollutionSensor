5e2443e49d5fafd581b2edf0e2928eb7ab416530
Lilypads <lilypads.lily@gmail.com>
2022-03-02 15:45:09 +0000

    fix main

--- example.cpp
+++ example.cpp
@@ -1,4 +1,11 @@
 #include "sqr.h"
-double sqr(double x)
-    {return x*x;}
+#include "iostream"
+//double sqr(double x)
+   // {return x*x;}
+
+int main()
+{
+std::cout << "Hello World!" << sqr(2) << std::endl;
+return 0;
+}
     
