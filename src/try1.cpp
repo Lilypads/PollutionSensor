0e384fd559e3e5d02f0f087c9aec7e4d383b8299
Lilypads <lilypads.lily@gmail.com>
2022-03-23 14:10:42 +0000

    Update try1.cpp

--- src/try1.cpp
+++ src/try1.cpp
@@ -15,7 +15,7 @@ char tmp[4];  //pointer & data to write
 	tmp[2] = (char)(( BIG_ENDIAN_IEEE754_FLOAT_TYPE & 0xff00) >> 8);
 	tmp[3] = (char)(DUMMY & 0x00ff);
 
-int i2cWriteDevice(handle,tmp,4)
+int out = i2cWriteDevice(handle,tmp,4);
 
 i2cClose(handle)
 }
@@ -33,7 +33,7 @@ char pnt[2];  //pointer
 	pnt[0] = (char)(( STOP_MEASUREMENT_B1 & 0xff00) >> 8);
 	pnt[1] = (char)(STOP_MEASUREMENT_B2 & 0x00ff);
 
-int i2cWriteDevice(handle,pnt,2)
+int out = i2cWriteDevice(handle,pnt,2);
 i2cClose(handle)
 
 }
@@ -53,8 +53,11 @@ char pnt[2];  //pointer
 
 char tmp[3];  //buffer for read data
 
-int i2cWriteDevice(handle,pnt,2)
-int i2cREADDevice(handle,tmp,3)
+int out = i2cWriteDevice(handle,pnt,2);
+int dataREAD = i2cREADDevice(handle,tmp,3);
+
+//tryyyyyy
+fprintf(stderr,"%d.\n",dataREAD);
 
 i2cClose(handle)
 
