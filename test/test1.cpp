d30a18cf45588bb848104f68a41402ff9deb3367
benjaminf <benjamin.frazer.1997@gmail.com>
2022-03-30 14:54:31 +0100

    fixed test1 build + moved example files out of src

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,12 +1,11 @@
 #define BOOST_TEST_MODULE testModuleName
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
-
+#include "sqr.h"
 //BOOST_WARN(1==1); //warn and continue
 //BOOST_CHECK(1>0); //fail but continue
 //BOOST_REQUIRE(1!=2); //fail and terminate
 
-#include "sqr.h"
 BOOST_AUTO_TEST_CASE(PassTest)
 {
     BOOST_CHECK_EQUAL(4, sqr(2));
