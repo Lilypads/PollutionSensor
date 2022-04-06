afb4b21706686ff97ea28d0c2f8d394a49ebbde6
benjaminf <benjamin.frazer.1997@gmail.com>
2022-04-06 01:19:45 +0100

    initial committal of GPS class

---
+++ src/GPS/neo6m.cpp
@@ -0,0 +1,194 @@
+#include "neo6m.h"
+
+NEO6M::NEO6M(neo6mSettings theseSettings){
+settings = theseSettings;
+}
+
+void NEO6M::startMeasurement(){
+#ifdef DEBUG
+    fprintf(stderr,"starting GPS acquisition...\n")
+#endif
+//spawn the acquisition thread
+
+if (nullptr != daqThread) {
+    // already running
+#ifdef DEBUG
+    fprintf(stderr,"starting GPS acquisition...\n")
+#endif
+return;
+}
+
+//start the polling thread
+daqThread = new std::thread(execPollingThread,this);
+};
+
+void NEO6M::stopMeasurement(){
+isPollingUart = false;
+//here we just wait untill the daq thread stops
+if (daqThread != nullptr){
+    daqThread->join();
+    delete daqThread;
+    daqThread = nullptr;
+    }
+close(fd);
+};
+
+void NEO6M::pollUartDev(){
+    char circBuff[NMEA_MAX_SENTENCE_SIZE*2]; //this way we can fit at least one full message in regardless of where we start
+    char data2Send[NMEA_MAX_SENTENCE_SIZE];
+    // not that i think the raspberry pi is 64 bit but i dont think my lsp know this
+    char* circBuffPtr;
+    uint16_t circBuffLatestElementPtr=0;
+    uint16_t circBuffDataStartPtr=0;
+    uint16_t circBuffIdxLast = circBuffLatestElementPtr + sizeof(circBuff);
+    uint16_t circBuffRemainingVerticalSpace;
+    int circBuffLatestStartChar =-1; //int so we can invalidate it
+
+    fd = open(settings.serialDevice.c_str(), O_RDONLY | O_NOCTTY | O_NDELAY);
+    configurePort(fd, settings.baudrate);
+    isPollingUart = true;
+    int nbytes = 0;
+    int dataLen;
+    uint lenFirstPart;
+    uint lenSecondPart;
+
+    while(isPollingUart){
+        // this is a sort of quasi ring buffer where we read as much data into
+        // the buffer beffore it wraps since read() can't write direcly into a
+        // ring buffer we just point it to the current next free element and
+        // ask for however many verticle elements are left before wrapping
+
+        //decide how many more bytes we can cram into our buffer
+        circBuffRemainingVerticalSpace = circBuffIdxLast-circBuffLatestElementPtr;
+        circBuffDataStartPtr%=sizeof(circBuff);//wrap the data start pointer
+        // read the byte into the buffer
+        nbytes = read(fd,circBuffPtr+circBuffLatestElementPtr,circBuffRemainingVerticalSpace);
+        if(nbytes<=0){
+            fprintf(stderr, "failed to read from port, read() returned %d\n", nbytes);
+        }
+        //itterate through these to see if we recieved a termination character
+        //note this will never wrap because we only ever ask for the remaining
+        //"vertical space" worth of bytes from read
+       for (;circBuffLatestElementPtr < circBuffLatestElementPtr+nbytes; circBuffLatestElementPtr++) {
+           // first we track if we encounter any message starts
+           if (circBuff[circBuffLatestElementPtr]==NMEA_SENTENCE_START_DELIM){
+               // we only ever need to track one of these because there should only be one per sentence
+               circBuffLatestStartChar = circBuffLatestElementPtr;
+           }
+           if (circBuff[circBuffLatestElementPtr]==NMEA_SENTENCE_END_DELIM){
+               if(circBuffLatestStartChar<0){
+                   fprintf(stderr, ) // FIXME
+               }
+               dataLen = circBuffLatestElementPtr - circBuffLatestStartChar;
+               if (dataLen<0){
+                   dataLen = circBuffLatestElementPtr + sizeof(circBuff)-circBuffDataStartPtr;
+                   lenFirstPart = circBuffDataStartPtr-sizeof(circBuff)-1;
+                   lenSecondPart = circBuffIdxLast; // compiler will optimise this one away
+                   memcpy(data2Send, circBuff+circBuffDataStartPtr,lenFirstPart);
+                   memcpy(data2Send+lenFirstPart, circBuff, lenSecondPart);
+                }
+               else{
+                   memcpy(data2Send, circBuff+circBuffDataStartPtr, dataLen);
+               }
+               if (dataLen>NMEA_MAX_SENTENCE_SIZE){
+                   fprintf(stderr, "Error: NMEA sentence length greater than Expected this will cause memory problems! (%d)",dataLen);
+               }
+               //now we have to reconstitute the ringbufferised sentence into the right order
+               hasNmeaSentance(data2Send, dataLen);
+               // increment the data start pointer by the data length
+               circBuffDataStartPtr +=dataLen;
+           }
+       }
+    }
+};
+
+int NEO6M::configurePort(int fd, int baud){
+// adapted from:
+// https://stackoverflow.com/questions/6947413/how-to-open-read-and-write-from-serial-port-in-c
+      struct termios tty;
+        if (tcgetattr (fd, &tty) != 0)
+        {
+                fprintf(stderr,"error %d from tcgetattr", errno);
+                return -1;
+        }
+
+        cfsetospeed(&tty, baud);
+        cfsetispeed(&tty, baud);
+
+        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+        // disable IGNBRK for mismatched speed tests; otherwise receive break
+        // as \000 chars
+        tty.c_iflag &= ~IGNBRK;         // disable break processing
+        tty.c_lflag = 0;                // no signaling chars, no echo,
+                                        // no canonical processing
+        tty.c_oflag = 0;                // no remapping, no delays
+        /*
+        * Im setting vmin to 1 here which means we read out bytes 1 at a
+        * time which is inefficient but there is no other way to make
+        * this code "real time" since the messages all have variable size
+        * and come in random order so if you were buffering bytes in the
+        * device, there is always a chance that the end of the message
+        * doesn't get to the end of the buffer so then you need to wait
+        * untill the next one comes allong before the user gets the
+        * callback
+            */
+        tty.c_cc[VMIN]  = 1;            // set to blocking buffer at least 1 char
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+
+        tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+        // enable reading
+        tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+        tty.c_cflag |= 0; //parity bit
+        tty.c_cflag &= ~CSTOPB;
+        tty.c_cflag &= ~CRTSCTS;
+
+        if (tcsetattr (fd, TCSANOW, &tty) != 0)
+        {
+               fprintf(stderr, "Failed to configure port");
+                return -1;
+        }
+        return 0;
+};
+
+bool NEO6M::testChecksum(char* sentance){
+// largely adapted from:
+  // https://github.com/craigpeacock/NMEA-GPS/blob/23f15158f124cc2b65c0468d5c690be23a449a77/gps.c#L109
+	char *checksum_str;
+	int returned_checksum;
+	int calculated_checksum = 0;
+
+	// Checksum is postcede by *
+	checksum_str = strchr(sentance, NMEA_SENTENCE_CHECKSUM_DELIM);
+	if (checksum_str != NULL){
+		// Remove checksum from sentance
+		*checksum_str = '\0';
+
+		// loop through remaining sentance to Calculate checksum, starting after $ (i = 1)
+		for (int i = 1; i < strlen(sentance); i++) {
+			calculated_checksum = calculated_checksum ^ sentance[i];
+		}
+
+        //returned checksum is apparently in hex characters
+		returned_checksum = NEO6M::hexChar2Int((char *)checksum_str+1);
+		if (returned_checksum == calculated_checksum) {
+			//printf("Checksum OK");
+			return 1;
+		}
+	} else {
+#ifdef DEBUG
+		fprintf(stderr,"Error: Checksum missing or NULL NMEA message\r\n");
+#endif
+		return false;
+	}
+	return 0;
+};
+
+int NEO6M::hexChar2Int(char* checksumChar){
+    // TODO write a test for this method should be fairly easy!!
+    int checksum;
+    // convert the msb and lsb into ints and then scale msb by a factor of 16
+    checksum = hexChar2IntLUT[(uint)(*checksumChar)]*16+hexChar2IntLUT[(uint)(*(checksumChar+1))];
+    return checksum;
+};
---
+++ src/GPS/neo6m.h
@@ -0,0 +1,95 @@
+#include <stdint.h>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <thread>
+#include <unistd.h>
+#include <poll.h>
+#include <string.h>
+#include <fcntl.h>
+#include <termios.h>
+
+
+#define NMEA_MAX_SENTENCE_SIZE 79+1 //+1 for null terminator
+#define NMEA_MAX_DATA_FIELD_SIZE 74 //+1 for null terminator
+#define CIRC_BUFF_SIZE NMEA_MAX_SENTENCE_SIZE*2
+#define NMEA_SENTENCE_START_DELIM '$'
+#define NMEA_SENTENCE_END_DELIM '\n'
+#define NMEA_SENTENCE_CHECKSUM_DELIM '*'
+#define NMEA_SENTENCE_DATA_DELIM ','
+#define NMEA_SENTENCE_MIN_SIZE 20 //this is just based on what ive seen
+#define NMEA_MAX_SENTANCES_IN_BUFF CIRC_BUFF_SIZE/NMEA_SENTENCE_MIN_SIZE
+#define SERIAL_DEVICE_STR_LEN 20
+#define NMEA_MAX_TRANMISSION_INTERVAL_S 2
+
+
+int hexChar2IntLUT[] =
+    {
+// ASCII
+//0  1   2   3   4   5   6   7   7   8   10 11  12  13  14  15
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 0-15
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 16-31
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 32-47
+ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, -1, -1, -1, -1, -1, -1, // 48-63
+-1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 64-79
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 80-95
+-1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 96-111
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 112-127
+};
+
+#ifndef NEO6M_H_
+#define NEO6M_H_
+
+struct neo6mSettings {
+    std::string serialDevice = "/dev/serial0";
+    int baudrate = 9600;
+};
+
+struct neo6mMeasurment{
+    float latt_deg;
+    float long_deg;
+    float alt_m;
+    float heading_deg;
+    float speed_mps;
+    float error2d_m;
+    float error3d_m;
+    float errorSpd_m;
+    bool dropout;
+};
+
+class NEO6M{
+//public properties
+    public:
+    //constructor
+    NEO6M(neo6mSettings theseSettings = neo6mSettings());
+    //destructor
+    ~NEO6M();
+
+    void startMeasurement();
+    void stopMeasurement();
+
+    neo6mMeasurment lastCompleteSample;
+    uint timeLastSample;
+    //user must implement this method!
+    virtual void hasMeasurementCB() = 0;
+
+//private methods
+    private:
+    int fd = -1; //file handle
+    void hasNmeaSentance(char* sentence, uint dataSize); // TODO //the polling method will call this once a full sentance has been recieved
+    int configurePort(int fd, int baud); // DONE
+    bool testChecksum(char* sentance); // DONE
+    int hexChar2Int(char* checksumChar); // DONE
+    neo6mMeasurment parseNmeaStr(); // TODO
+    void pollUartDev(); // TODO
+    static void execPollingThread(NEO6M* thisClassPtr){
+        thisClassPtr->pollUartDev();
+    }
+    bool isPollingUart=0;
+    std::thread* daqThread = nullptr;
+    neo6mSettings settings;
+
+};
+
+
+#endif // NEO6M_H_
