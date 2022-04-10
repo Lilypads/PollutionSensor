6dd9240104d36d229997db4ddd138d57c7b1ce8e
Lilypads <lilypads.lily@gmail.com>
2022-04-10 23:44:18 +0100

    initial commit

--- src/StorageHandler/storageHandler.h
+++ src/StorageHandler/storageHandler.h
@@ -1,2 +1,52 @@
+#include <iostream>
+#include <fstream>
 #include "neo6m.h"
 #include "libsps30.h"
+
+class storageHandler {
+    public:
+	/**
+	 * Constructor
+	 **/
+	storageHandler();
+
+	/**
+	 * Destructor 
+	 **/
+	~storageHandler();
+
+    /**
+	 * close all files
+	 **/
+    void closeFiles();
+
+    /**
+	 * create files
+	 **/
+    void createFiles(int identificationNumber);
+
+    /**
+	 * write to files
+	 **/
+    void writeToFile(SPS30measurement currentMeasurement);
+
+    /**
+	 * GPS buffer
+	 **/
+    neo6mMeasurment bufferMeasurement;
+
+    private:
+            /*
+            * syntax documentation
+            */
+            //write only
+            //fstream signal_file;
+            //read and write
+            //ifstream signal_file;
+
+        fstream logdata_file;
+
+}
+
+
+    
