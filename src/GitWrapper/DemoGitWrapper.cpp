3db04605a8074e7d5745411f3d5e28ece8fac08e
benjaminf <benjamin.frazer.1997@gmail.com>
2022-04-14 20:40:16 +0100

    some final fixes to build tool chain

--- src/GitWrapper/DemoGitWrapper.cpp
+++ src/GitWrapper/DemoGitWrapper.cpp
@@ -1,10 +1,9 @@
 #include "gitWrapper.h"
-char thisJourneyFile[] = "testJourney.pol";
 int main(){
     gitwrapperSettings s;
     strcpy(s.path, "/home/benjaminf/Repos/PollutionSensorDataDir");
     strcpy(s.remote, "git@github.com:BodeanTheZealous/PollutionSensorData.git");
     strcpy(s.sshKeysFileAbs, "~/Repos/PollutionSensor/src/GitWrapper/pollSenseKey");
     GITWRAPPER gw(s);
-    gw.saveJoureyFile(thisJourneyFile);
+    gw.saveJoureyFiles();
 }
